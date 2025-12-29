772864b1a1b370264e104cf9462fdd465df95240
LetnanRaffi <muhamadraffipasha1611@gmail.com>
2025-12-29 21:33:24 +0700

    YA YA YA

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Y7uvTTerkJVJ1CdsF0sD+pHSJ+zwBVofWgwUljKM+3o=\"\n}"
--- .next/dev/static/chunks/app_page_tsx_6bece898._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_f50cc847._.js",
-  "static/chunks/node_modules_7487cf6d._.js"
-],
-    source: "dynamic"
-});
